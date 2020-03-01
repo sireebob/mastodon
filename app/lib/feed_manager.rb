b46dc2da243119414cb14ba3b74060f441da81b9
Thibaut Girka <thib@sitedethib.com>
2020-03-01 15:21:21 +0100

    Fix query

--- app/lib/feed_manager.rb
+++ app/lib/feed_manager.rb
@@ -222,7 +222,7 @@ class FeedManager
     # hometown: exclusive list rules
     unless timeline_type == :list
       # find all exclusive lists
-      @list = List.where(account: Account.find(receiver_id), is_exclusive: true)
+      @list = List.where(account_id: receiver_id, is_exclusive: true)
       # is there a list the receiver owns with this account on it? if so, return true
       return true if ListAccount.where(list: @list, account_id: status.account_id).exists?
     end
