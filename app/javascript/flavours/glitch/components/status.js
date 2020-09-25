00936a79b2cecee9dacf6d94f2b5a7f4180b6420
sireebob <38407957+sireebob@users.noreply.github.com>
2020-09-25 00:06:10 -0700

    support glitch-soc flavours for preview cards on statuses with cw's.

--- app/javascript/flavours/glitch/components/status.js
+++ app/javascript/flavours/glitch/components/status.js
@@ -652,7 +652,7 @@ class Status extends ImmutablePureComponent {
         mediaIcon = 'picture-o';
       }
 
-      if (!status.get('sensitive') && !(status.get('spoiler_text').length > 0) && settings.getIn(['collapsed', 'backgrounds', 'preview_images'])) {
+      if (!status.get('sensitive') && settings.getIn(['collapsed', 'backgrounds', 'preview_images'])) {
         background = attachments.getIn([0, 'preview_url']);
       }
     } else if (status.get('card') && settings.get('inline_preview_cards')) {
